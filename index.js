1209d01081fd0b90782ea031024d753b4e86bf8c
bdiazc90 <bdiazc90@gmail.com>
2025-04-05 10:29:45 -0500

    class 12

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import movieRoutes from "./routes/MovieRoutes.js";
+import cors from "cors"; // Middleware para permitir CORS de un tercero
 
 import { helloWorld } from "./controllers/RootController.js";
 import { logginRequest } from "./middlewares/loggin.js";
 
 const app = express();
 const port = 3000;
+
+app.use(cors()); // Middeware para permitir CORS
 
 app.get("/", logginRequest, helloWorld);
 
